7f13b6740a8468e8efe91cbcd9dde74765bb380d
Rose <zumruttowel@gmail.com>
2025-09-16 05:47:24 -0700

    my first module

--- 1.py
+++ 1.py
@@ -1,1 +1,59 @@
-print("hello world")
+class admin:
+    def __init__(self,username,password):
+        pass
+    def authenticate(self, input_username, input_password):
+        pass
+class admin_panel:
+    def __init__(self):
+        pass
+    def add_employee(self,name,lastname,password,username):
+        pass
+    def delet_employee(self,username):
+        pass
+    def employeed_list(self):
+        pass
+    def log_out(self):
+        pass
+class train_employee:
+    def __init__(self,username,password):
+        pass
+    def authenticate(self, input_username, input_password):
+        pass
+class empoloyee_panel:
+    def __init__(self):
+        pass
+    def add_line(self,lines_name,origin,destanition):
+        pass
+    def update_lines_info(self,lines_name):
+        pass
+    def delet_line(self,lines_name):
+        pass
+    def lines_list(self):
+        pass
+    def add_train(self,trains_name,move_line,av_v,stop_rate,stations,quality_grade,ticket_price,train_capacity,id):
+        pass
+    def update_trian_info(self,id):
+        pass
+    def delet_train(self,id):
+        pass
+    def trains_list(self):
+        pass
+    def log_out(self):
+        pass
+class normal_user:
+    def __init__(self):
+        pass
+    def sign_in(self,name,user_name,password,email):
+        pass
+    def log_in(self,user_name,password):
+        pass
+    def log_out(self):
+        pass
+class shopping:
+    def __init__(self):
+        pass
+    def wallet(self):
+        pass
+    def edit_info(self):
+        pass
+
